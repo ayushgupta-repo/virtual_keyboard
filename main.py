a1ac191354ae7bf02a3e2891270b4ed890a95f99
ayushgupta-repo <19ce037@charusat.edu.in>
2022-02-25 02:44:58 +0530

    Creating handdetection using HandTrackingModule and exit creation

--- main.py
+++ main.py
@@ -6,15 +6,34 @@
 
 # importing packages and libraries
 import cv2
+from HandTrackingModule import handDetector
 
 # using webcam as it's id is 0 to create video capturing device
 cap = cv2.VideoCapture(0)
 
 # resizing video capturing dimensions
-cap.set(3, 1280)
-cap.set(4, 720)
+cap.set(3, 1280)  # width
+cap.set(4, 720)  # height
+
+# creating handdetector object
+
+# providing higher confidence value as by mistake key does not get pressed
+detector = handDetector(detectionCon=0.8)
 
 while True:
     success, img = cap.read()
+
+    # finding hands from the image/webcam
+    img = detector.findHands(img)
+
+    lmList = detector.findPosition(img, False)
+
     cv2.imshow('Image', img)
     cv2.waitKey(1)
+
+    # q button pressing to exit the prompt
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()

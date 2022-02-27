f639dc1530e3ddfa67209bc27908c2bfe3007639
ayushgupta-repo <19ce037@charusat.edu.in>
2022-02-28 01:36:26 +0530

    Creating buttons on image still incomplete

--- main.py
+++ main.py
@@ -20,6 +20,25 @@ cap.set(4, 720)  # height
 # providing higher confidence value as by mistake key does not get pressed
 detector = handDetector(detectionCon=0.8)
 
+
+class Button():
+    def __init__(self, pos, text, size=[85, 85]):
+        self.pos = pos
+        self.size = size
+        self.text = text
+        x, y = self.pos
+        w, h = self.size
+        # Creating rectangular boxes that contains font
+        cv2.rectangle(img, self.pos, (x+w, y+h), (255, 0, 255), cv2.FILLED)
+
+        # putting text on the image
+        cv2.putText(img, self.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN,
+                    4, (255, 255, 255), 4)
+
+
+# Creating button list
+buttonList = []
+
 while True:
     success, img = cap.read()
 
@@ -27,6 +46,14 @@ while True:
     img = detector.findHands(img)
 
     lmList = detector.findPosition(img, False)
+
+    # Creating buttons object with loop
+    for x in range(0, 5):
+        buttonList.append(Button([100*x+50, 100], "Q"))
+
+    # img = myButton.draw(img)
+    # img = myButton1.draw(img)
+    # img = myButton2.draw(img)
 
     cv2.imshow('Image', img)
     cv2.waitKey(1)

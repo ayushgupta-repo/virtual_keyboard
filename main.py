4543e2ea1ba71c773cba4aad4cae88735d5f5c3b
ayushgupta-repo <19ce037@charusat.edu.in>
2022-03-29 11:48:56 +0530

    Edit in main.py file

--- main.py
+++ main.py
@@ -25,8 +25,6 @@ keys = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
         ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';'],
         ['Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/']]
 
-# to draw
-
 
 def drawALL(img, buttonList):
 
